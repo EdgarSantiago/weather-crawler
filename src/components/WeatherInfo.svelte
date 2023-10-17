c6628e50c4b56b3254bae3d9dcaefff3f1a5725d
EdgarSantiago <edgargsp777@gmail.com>
2023-10-16 23:32:38 -0300

    weather info component

---
+++ src/components/WeatherInfo.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let title = "Prob. de Chuva";
+  export let percentage = "26%";
+  export let rainfall = "5mm";
+</script>
+
+<div class="flex justify-between w-full text-2xl mx-auto p-2 rounded-3xl">
+  <p>{title}</p>
+  <p>{percentage}</p>
+</div>
--- src/routes/temperature/[cityname]/+page.svelte
+++ src/routes/temperature/[cityname]/+page.svelte
@@ -1,5 +1,8 @@
 <script>
   // @ts-nocheck
+
+  import WeatherInfo from "../../../components/WeatherInfo.svelte";
+  // @ts-nocheck
   import { page } from "$app/stores";
   import { onMount } from "svelte";
 
@@ -23,7 +26,31 @@
     <h1 class="text-3xl lg:text-7xl">crawler de previsão !!!</h1>
   </div>
 
-  <a href={`${href}`} class="text-4xl">
-    {cityname}
-  </a>
+  <div
+    class="max-w-md mx-auto p-5 rounded-3xl border-4 flex flex-col gap-2 text-start"
+  >
+    <!-- about -->
+    <p class="text-2xl">Hoje, sábado</p>
+    <p class="text-4xl">
+      {cityname}
+    </p>
+    <p class="text-2xl">
+      Sol com muitas nuvens de manhã e pancadas de chuva à tarde. À noite a
+      chuva para.
+    </p>
+
+    <!-- min - max -->
+    <div class="flex gap-5">
+      <p class="text-2xl text-blue-400 rounded-3xl">18ºmin</p>
+      <p class="text-2xl text-red-400 rounded-3xl">34ºmáx</p>
+    </div>
+
+    <!-- data -->
+    <div class="flex flex-col gap-5">
+      <WeatherInfo title="Prob. de Chuva" percentage="11% • 0mm" />
+      <WeatherInfo title="Nascer do Sol	" percentage="05:49" />
+      <WeatherInfo title="Pôr do Sol" percentage="18:17" />
+      <WeatherInfo title="Vento" percentage="22km/h • ENE" />
+    </div>
+  </div>
 </section>
